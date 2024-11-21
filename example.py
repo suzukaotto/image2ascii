279cf9646dc7afdfe956b93743acb65f01c2933b
suzukaotto <me@szk.kr>
2024-11-21 19:58:02 +0900

    update: example logo updated

--- example.py
+++ example.py
@@ -2,7 +2,8 @@ from image2ascii import image2ascii
 
 image_path = r'src/image.png'
 width = 100
-height = 25
+height = 20
+ascii_chars = "# "
 
-ascii_art = image2ascii(image_path, width, height, black=" ")
+ascii_art = image2ascii(image_path, width, height, ascii_chars, black=" ")
 print(ascii_art)
